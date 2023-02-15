c98bb7d497b968d2a69b92b24fbd0bee163dcd9a
Steph123123 <steph13am@gmail.com>
2023-02-15 16:54:38 +0000

    holi

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import List from "./list.jsx";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -6,19 +7,22 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		
+		<div className="container mt-5 todo">
+			<div className="row">
+				<h1 className="text-center mt-5">Steph´s to do list with fetch!</h1>
+				<div className="col-12">
+					<List />
+				</div>
+				<div className="img img-fluid">
+					<img
+						src="https://creazilla-store.fra1.digitaloceanspaces.com/emojis/42676/t-rex-emoji-clipart-md.png"
+						alt=""
+					/>
+					
+				</div>
+			</div>
+		
 		</div>
 	);
 };
